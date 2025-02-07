43617155fb1014161982e774c17d6c7c570de81a
Roman Guivan <tehg4rden@gmail.com>
2025-02-07 19:43:46 +0100

    fight dumb context path

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  let isProduction = process.env.ELEVENTY_ENV === "production";
   return {
+    pathPrefix: isProduction === "production" ? "/interfilm-ua-vhs/" : "/",
     dir: {
       input: "src",
       output: "dist"
